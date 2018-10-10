12beb40dc91f36f0166913d4d29a86eda3dfa4d4
Bas des Tombe <bdestombe@gmail.com>
2018-10-10 16:40:58 +0200

    Add open gef datastore routine to read the object from,e.g., netCDF

--- src/geotechnicalprofile/gp_datastore.py
+++ src/geotechnicalprofile/gp_datastore.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import inspect
 import tempfile
 from datetime import datetime as dt
 from itertools import cycle
@@ -180,6 +181,106 @@ class DataStore(xr.Dataset):
         self[gef_tmp_label] = (dims, btmp_data, dss[dts_tmp_label].attrs)
 
 
+def open_datastore(filename_or_obj, group=None, decode_cf=True,
+                   mask_and_scale=None, decode_times=True, autoclose=False,
+                   concat_characters=True, decode_coords=True, engine=None,
+                   chunks=None, lock=None, cache=None, drop_variables=None,
+                   backend_kwargs=None, **kwargs):
+    """Load and decode a datastore from a file or file-like object.
+    Parameters
+    ----------
+    filename_or_obj : str, Path, file or xarray.backends.*DataStore
+        Strings and Path objects are interpreted as a path to a netCDF file
+        or an OpenDAP URL and opened with python-netCDF4, unless the filename
+        ends with .gz, in which case the file is gunzipped and opened with
+        scipy.io.netcdf (only netCDF3 supported). File-like objects are opened
+        with scipy.io.netcdf (only netCDF3 supported).
+    group : str, optional
+        Path to the netCDF4 group in the given file to open (only works for
+        netCDF4 files).
+    decode_cf : bool, optional
+        Whether to decode these variables, assuming they were saved according
+        to CF conventions.
+    mask_and_scale : bool, optional
+        If True, replace array values equal to `_FillValue` with NA and scale
+        values according to the formula `original_values * scale_factor +
+        add_offset`, where `_FillValue`, `scale_factor` and `add_offset` are
+        taken from variable attributes (if they exist).  If the `_FillValue` or
+        `missing_value` attribute contains multiple values a warning will be
+        issued and all array values matching one of the multiple values will
+        be replaced by NA. mask_and_scale defaults to True except for the
+        pseudonetcdf backend.
+    decode_times : bool, optional
+        If True, decode times encoded in the standard NetCDF datetime format
+        into datetime objects. Otherwise, leave them encoded as numbers.
+    autoclose : bool, optional
+        If True, automatically close files to avoid OS Error of too many files
+        being open.  However, this option doesn't work with streams, e.g.,
+        BytesIO.
+    concat_characters : bool, optional
+        If True, concatenate along the last dimension of character arrays to
+        form string arrays. Dimensions will only be concatenated over (and
+        removed) if they have no corresponding variable and if they are only
+        used as the last dimension of character arrays.
+    decode_coords : bool, optional
+        If True, decode the 'coordinates' attribute to identify coordinates in
+        the resulting dataset.
+    engine : {'netcdf4', 'scipy', 'pydap', 'h5netcdf', 'pynio', 'pseudonetcdf'}, optional
+        Engine to use when reading files. If not provided, the default engine
+        is chosen based on available dependencies, with a preference for
+        'netcdf4'.
+    chunks : int or dict, optional
+        If chunks is provided, it used to load the new dataset into dask
+        arrays. ``chunks={}`` loads the dataset with dask using a single
+        chunk for all arrays.
+    lock : False, True or threading.Lock, optional
+        If chunks is provided, this argument is passed on to
+        :py:func:`dask.array.from_array`. By default, a global lock is
+        used when reading data from netCDF files with the netcdf4 and h5netcdf
+        engines to avoid issues with concurrent access when using dask's
+        multithreaded backend.
+    cache : bool, optional
+        If True, cache data loaded from the underlying datastore in memory as
+        NumPy arrays when accessed to avoid reading from the underlying data-
+        store multiple times. Defaults to True unless you specify the `chunks`
+        argument to use dask, in which case it defaults to False. Does not
+        change the behavior of coordinates corresponding to dimensions, which
+        always load their data from disk into a ``pandas.Index``.
+    drop_variables: string or iterable, optional
+        A variable or list of variables to exclude from being parsed from the
+        dataset. This may be useful to drop variables with problems or
+        inconsistent values.
+    backend_kwargs: dictionary, optional
+        A dictionary of keyword arguments to pass on to the backend. This
+        may be useful when backend options would improve performance or
+        allow user control of dataset processing.
+    Returns
+    -------
+    dataset : Dataset
+        The newly created dataset.
+    See Also
+    --------
+    read_xml_dir
+    """
+
+    xr_kws = inspect.signature(xr.open_dataset).parameters.keys()
+
+    ds_kwargs = {k: v for k, v in kwargs.items() if k not in xr_kws}
+
+    ds_xr = xr.open_dataset(
+        filename_or_obj, group=group, decode_cf=decode_cf,
+        mask_and_scale=mask_and_scale, decode_times=decode_times, autoclose=autoclose,
+        concat_characters=concat_characters, decode_coords=decode_coords, engine=engine,
+        chunks=chunks, lock=lock, cache=cache, drop_variables=drop_variables,
+        backend_kwargs=backend_kwargs)
+
+    ds = DataStore(data_vars=ds_xr.data_vars,
+                   coords=ds_xr.coords,
+                   attrs=ds_xr.attrs,
+                   **ds_kwargs)
+    return ds
+
+
 def read_gef(fp):
     """
     Read a GEF file
@@ -276,7 +377,7 @@ def read_gef(fp):
 
     _date = attrs['STARTDATE'] + attrs['STARTTIME']
     date = [int(item) for item in _date]
-    attrs['date'] = pd.to_datetime(dt(*date), utc=True)
+    attrs['date'] = str(pd.to_datetime(dt(*date), utc=True))
     del attrs['STARTDATE']
     del attrs['STARTTIME']
 
